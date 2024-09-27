f564822cb7b7d3fdf5adad624c956297396695d1
ind0zer <165696434+ind0zer@users.noreply.github.com>
2024-09-28 00:42:19 +0300

    done

--- main.js
+++ main.js
@@ -34,39 +34,39 @@ document.addEventListener("keydown", (e) => {
 });
 
 function createBoxes(amount) {
-    const container = document.querySelector("#boxes");
-    let size = 30; 
-  
-    for (let i = 0; i < amount; i++) {
-      const box = document.createElement("div");
-      box.style.width = `${size}px`;
-      box.style.height = `${size}px`;
-      box.style.backgroundColor = getRandomColor();
-      box.style.marginBottom = "10px"; 
-      container.appendChild(box);
-      
-      size += 10; 
-    }
-  }
+  const container = document.querySelector("#boxes");
+  const currentBoxCount = container.children.length;
+  let size = 30 + currentBoxCount * 10;
 
-  function getRandomColor() {
-    const r = Math.floor(Math.random() * 256);
-    const g = Math.floor(Math.random() * 256);
-    const b = Math.floor(Math.random() * 256);
-    return `rgb(${r}, ${g}, ${b})`;
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomColor();
+    box.style.marginBottom = "10px";
+    container.appendChild(box);
+
+    size += 10;
   }
-  
-  function destroyBoxes() {
-    const container = document.querySelector("#boxes");
-    container.innerHTML = '';
+}
+
+function getRandomColor() {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  return `rgb(${r}, ${g}, ${b})`;
+}
+
+function destroyBoxes() {
+  const container = document.querySelector("#boxes");
+  container.innerHTML = '';
+}
+
+document.querySelector("[data-action='render']").addEventListener("click", () => {
+  const amount = document.querySelector("#amount").value;
+  if (amount > 0) {
+    createBoxes(amount);
   }
-  
-  document.querySelector("[data-action='render']").addEventListener("click", () => {
-    const amount = document.querySelector("#amount").value;
-    destroyBoxes();
-    if (amount > 0) {
-      createBoxes(amount);
-    }
-  });
-  
-  document.querySelector("[data-action='destroy']").addEventListener("click", destroyBoxes);
+});
+
+document.querySelector("[data-action='destroy']").addEventListener("click", destroyBoxes);
